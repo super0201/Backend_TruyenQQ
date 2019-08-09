e0c427b8d9f18f7b26f38badb8ddddc3066b2b97
John Nguyen <34763517+super0201@users.noreply.github.com>
2019-08-09 22:21:14 +0700

    Update api_register.php

--- api_for_user/api_register.php
+++ api_for_user/api_register.php
@@ -9,24 +9,24 @@
 include '../function.php';
 header('Content-Type: application/json');
 
-$user = (!empty($_POST['user']))?$_POST['user']: NULL;
-$pass = (!empty($_POST['pass']))?$_POST['pass']: NULL;
-$name = (!empty($_POST['name']))?$_POST['name']: NULL;
-$date = (!empty($_POST['date']))?$_POST['date']: 0;
+$user = (!empty($_GET['user']))?$_GET['user']: NULL;
+$pass = (!empty($_GET['pass']))?$_GET['pass']: NULL;
+$name = (!empty($_GET['name']))?$_GET['name']: NULL;
+$date = (!empty($_GET['date']))?$_GET['date']: 0;
 
 
 if ($user != NULL && $pass != NULL && $name != NULL && $date != 0) {
     if (matchUsername($user)) {
-        $response["result"] = "failure";
+        $response["result"] = "0";
         $response["message"] = "User Already Registered !";
     } else {
         userRegister($user, $pass, $name, $date);
         
-        $response["result"] = "success";
+        $response["result"] = "1";
         $response["message"] = "User Registered Successfully !";
     }
 } else {
-    $response['result'] = "failure";
+    $response['result'] = "0";
     $response['message'] = "Username, Password, Name, Date_add can't be empty";
 }
 
